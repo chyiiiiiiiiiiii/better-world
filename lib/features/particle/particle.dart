58e944af302ff29de7aad8eba6b5f469a1a4fe3e
Yii Chen <ab20803@gmail.com>
2024-02-13 00:38:41 +0800

    Fix(macos): fix firebase integration of macos system

--- lib/features/particle/particle.dart
+++ lib/features/particle/particle.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:confetti/confetti.dart';
-import 'package:envawareness/controllers/play_controller.dart';
+import 'package:envawareness/features/play/play_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
